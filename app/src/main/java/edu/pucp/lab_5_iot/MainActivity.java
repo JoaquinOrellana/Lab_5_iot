8edae5c96f265941ec6af94e1a73c84ba5fec107
victordzx <diaz.victorm@pucp.edu.pe>
2022-11-18 09:46:24 -0500

    firebase (save img)

--- app/src/main/java/edu/pucp/lab_5_iot/MainActivity.java
+++ app/src/main/java/edu/pucp/lab_5_iot/MainActivity.java
@@ -1,14 +1,67 @@
 package edu.pucp.lab_5_iot;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+
+    // Create a storage reference from our app
+    StorageReference storageRef = storage.getReference();
+
+    // Create a reference with an initial file path and name
+    StorageReference pathReference = storageRef.child("images/capybara-4254528__480.jpg");
+
+    // Create a reference to a file from a Google Cloud Storage URI
+    StorageReference gsReference = storage.getReferenceFromUrl("gs://bucket/images/capybara-4254528__480.jpg");
+
+    // Create a reference from an HTTPS URL
+// Note that in the URL, characters are URL escaped!
+    StorageReference httpsReference = storage.getReferenceFromUrl("https://firebasestorage.googleapis.com/b/bucket/o/capybara-4254528__480.jpg");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StorageReference islandRef = storageRef.child("images/capybara-4254528__480.jpg");
+
+        File localFile = null;
+        try {
+            localFile = File.createTempFile("images", "jpg");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        islandRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                // Local temp file has been created
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
     }
 }
