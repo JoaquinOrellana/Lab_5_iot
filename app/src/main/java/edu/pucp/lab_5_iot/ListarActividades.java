485d4fd7243f9eb21609d530cfafc03b1a18f607
KatherineObregon <a20181823@pucp.edu.pe>
2022-11-18 09:21:03 -0500

    lista actividades

---
+++ app/src/main/java/edu/pucp/lab_5_iot/ListarActividades.java
@@ -0,0 +1,17 @@
+package edu.pucp.lab_5_iot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListarActividades extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_actividades);
+
+
+
+    }
+}
